8a38555fc17986b957be22be3c2ba80c62f9ae8d
Ovidiu Rata <ovidiurata03@gmail.com>
2024-04-13 13:33:19 +0300

    Added MyAccount page and console logs for submit buttons

---
+++ js/app.js
@@ -0,0 +1,21 @@
+function loginButtonPressed(){
+  let username = document.getElementById("username-input").value
+  let password = document.getElementById("password-input").value
+  console.log("Inputted username: "+username);
+  console.log("Inputted password: "+password);
+}
+
+
+function signUpButtonPressed(){
+  let username = document.getElementById("username-input").value
+  let fullName = document.getElementById("fullname-input").value
+  let email = document.getElementById("email-input").value
+  let password = document.getElementById("password-input").value
+  let confirmPassword = document.getElementById("confirm-password-input").value
+  console.log("Inputted username: "+username);
+  console.log("Inputted fullname: "+fullName);
+  console.log("Inputted email: "+email);
+  console.log("Inputted password: "+password);
+  console.log("Inputted confirm password: "+confirmPassword);
+
+}
