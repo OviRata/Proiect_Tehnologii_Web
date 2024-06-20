dae18354b4df1b552005202599f1dcde63ff7907
Ovidiu Rata <ovidiurata03@gmail.com>
2024-06-20 14:36:35 +0300

    element hiding script added

---
+++ resources/js/menuByRole.js
@@ -0,0 +1,52 @@
+function hideElement( element ){
+  if(element===null)return;
+  element.style.display="none";
+}
+
+
+function hideForClient(){
+    //document.getElementById("");
+  hideElement(document.getElementById("list-mygarden"));
+
+  hideElement(document.getElementById("list-mygarden1"));
+}
+
+function hideForVendor(){
+  hideElement(document.getElementById("list-mycart"));
+  hideElement(document.getElementById("list-search"));
+
+  hideElement(document.getElementById("add-to-cart"));
+
+  hideElement(document.getElementById("list-mycart1"));
+  hideElement(document.getElementById("list-search1"));
+}
+
+function hideForNotLogged(){
+  hideElement(document.getElementById("list-mycart"));
+  hideElement(document.getElementById("list-mygarden"));
+
+
+  hideElement(document.getElementById("add-to-cart"));
+
+  hideElement(document.getElementById("list-mycart1"));
+  hideElement(document.getElementById("list-mygarden1"));
+}
+
+
+function hideForRole(role){
+  console.log("hiding for "+globalRole);
+  if(role==="client"){
+    hideForClient();
+  }
+  if(role==="vendor"){
+    hideForVendor();
+  }
+  if(role==="notlogged") {
+    hideForNotLogged();
+  }
+}
+
+
+// document.addEventListener(onload, ()=>{hideForRole(globalRole)} );
+// hideForRole(globalRole)
+
