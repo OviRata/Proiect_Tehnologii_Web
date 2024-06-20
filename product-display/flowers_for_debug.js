9cb217a06c12122941eb13541dbf0676c942c26d
Ovidiu Rata <ovidiurata03@gmail.com>
2024-06-20 13:53:14 +0300

    plant conditions added

--- product-display/flowers_for_debug.js
+++ product-display/flowers_for_debug.js
@@ -1,7 +1,6 @@
  var products = [
-  {userID:2, name: "Iridarium", image: "./product-display/flowers/iridarium.jpg", price: "$10.00", description: "Description of Product 1"  },
-  { userID:1, name: "Lalele", image: "./product-display/flowers/lalele.jpg", price: "$15.00", description: "Description of Product 1"  },
-  { userID:0, name: "Hortesia", image: "./product-display/flowers/hortesia.jpg", price: "$26.00" , description: "Description of Product 1" },
-  { userID:0, name: "Limba mielului", image: "./product-display/flowers/limba_mielului_2.jpg", price: "$12.00", description: "Description of Product 1"  },
-
+  { productID:1, userID:2, name: "Iridarium", image: "./product-display/flowers/iridarium.jpg", price: "$10.00", soil:"clay", temperature:"27 °C",humidity:"80%", water:"5" , description: "Description of Product 1"  },
+  { productID:2, userID:1, name: "Lalele", image: "./product-display/flowers/lalele.jpg", price: "$15.00", soil:"clay", temperature:"27 °C",humidity:"80%", water:"5" , description: "Description of Product 1"  },
+  { productID:3, userID:0, name: "Hortesia", image: "./product-display/flowers/hortesia.jpg", price: "$26.00" , soil:"clay", temperature:"27 °C",humidity:"80%", water:"5" , description: "Description of Product 1" },
+  { productID:4, userID:0, name: "Limba mielului", image: "./product-display/flowers/limba_mielului_2.jpg", price: "$12.00", soil:"clay", temperature:"27 °C",humidity:"80%", water:"5" , description: "Description of Product 1"  },
 ];
