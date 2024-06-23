885a7b9fddaa790434a0b76187f7f73bf077990f
Ovidiu Rata <ovidiurata03@gmail.com>
2024-06-23 18:36:10 +0300

    server adapted

--- controllers/sensorController.js
+++ controllers/sensorController.js
@@ -1,4 +1,28 @@
+const {getBodyFromRequest} = require("../bodyParser");
+const {sendJson} = require("../utilities");
+const User = require("../db/model/User");
+
+const Notification = require("../db/model/Notification");
+const {sendJson} = require("../utilities");
+
 const getNotificationFromSensor = async (req, res)=>{
+
+  const requestObject = await getBodyFromRequest(req);
+  req.body=JSON.parse(requestObject);
+
+  if( !req?.body?.userID || !req?.body?.content ){
+    return sendJson(res, 409, 'Bad request');
+  }
+
+  const userID = req.body.userID;
+  const content = req.body.content;
+
+  const result = await Notification.create({
+    'userID':userID,
+    'content':content
+  });
+
+  return sendJson(res, 200, {message:'notification received successfully'} );
 
 }
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const {sendJson} = require("../utilities");
 const User = require('../db/model/User');
 const {getBodyFromRequest} = require("../bodyParser");
 const {generateToken} = require("../authentification/jwt");
-
+const Notification = require('../db/model/Notification');
 
 const changeUserInfo = async (req, res) =>{
   if(!req?.user?.username){
@@ -30,4 +30,42 @@ const changeUserInfo = async (req, res) =>{
   return sendJson(res, 200, {message: "User info changed successfully", jwt:jwt});
 }
 
-module.exports={changeUserInfo};
+
+const getUserNotifications = async(req, res)=>{
+  if(!req?.user?.username){
+    return sendJson(res, 409, {error:"bad request"});
+  }
+  user = await User.findOne({ username: req.user.username });
+  if(!user){
+    return sendJson(res, 409, {error:"User not found"});
+  }
+
+  const userID = user._id;
+
+  const notifications = await Notification.find({userID:userID});
+
+  sendJson(res,200, {notifications:notifications} );
+
+}
+
+const deleteUserNotification = async( req, res)=>{
+  if(!req?.user?.username){
+    return sendJson(res, 409, {error:"bad request"});
+  }
+  user = await User.findOne({ username: req.user.username });
+  if(!user){
+    return sendJson(res, 409, {error:"User not found"});
+  }
+
+  const notificationID = req.headers['notificationID'];
+  const notification = await Notification.findOne({ _id: notificationID });
+  if(!notification){
+    return sendJson(res, 409, {error:"Notification with such id not found"});
+  }
+
+  const result = await Notification.deleteOne({_id:notificationID});
+
+  sendJson(res, 200, {message:'Notification deleted successfully'});
+}
+
+module.exports={changeUserInfo, getUserNotifications, deleteUserNotification};
