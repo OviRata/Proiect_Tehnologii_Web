d3f8b647942e3d870f95fc012a1b97a39f51a5f1
adiaioana <adiaioana@gmail.com>
2024-06-24 15:09:42 +0300

    added security for placing order, whitelist for GET file reqs

---
+++ whitelist.js
@@ -0,0 +1,86 @@
+const pathsCompletelyWhiteListed=[
+  "/product-display/flowers/",
+  "/resources/img/",
+]
+const whitelist = [
+  "/resources/favicon.ico",
+  "/resources/icon.png",
+  "/resources/icon.svg",
+  "/resources/notification-icon.png",
+  "/product-display/flowersflower1.jpg",
+  "/",
+  "/404.html",
+  "/bodyParser.js",
+  "/ChangePassword.html",
+  "/homePageClient.html",
+  "/homePageVendor.html",
+  "/index.html",
+  "/login",
+  "/register",
+  "/mygarden.html",
+  "/myshoppingcart.html",
+  "/placeOrder.html",
+  "/plantFlower.html",
+  "/product.html",
+  "/removeFlower.html",
+  "/Report.html",
+  "/search.html",
+  "/server.js",
+  "/utilities.js",
+  "/webpack.common.js",
+  "/webpack.config.dev.js",
+  "/webpack.config.prod.js",
+  "/whitelist.js",
+  "/accountpages/MyAccount.html",
+  "/accountpages/MyAccountNotLoggedIn.html",
+  "/accountpages/mynotifications/MyNotifications.html",
+  "/login+register/formValidation.js",
+  "/login+register/login.html",
+  "/login+register/login_register_logic.js",
+  "/login+register/register.html",
+  "/product-display/flowers_for_debug.js",
+  "/product-display/products-grid.js",
+  "/product-display/search.js",
+  "/resources/css/formContainer.css",
+  "/resources/css/homePageVendor.css",
+  "/resources/css/menustyle.css",
+  "/resources/css/mygarden.css",
+  "/resources/css/notifications_styles.css",
+  "/resources/css/plantFlower.css",
+  "/resources/css/product-card.css",
+  "/resources/css/productpage-styles.css",
+  "/resources/css/profileContainer.css",
+  "/resources/css/search.css",
+  "/resources/css/shoppingcart.css",
+  "/resources/css/style.css",
+  "/resources/js/app.js",
+  "/resources/js/config.js",
+  "/resources/js/export.js",
+  "/resources/js/menuByRole.js",
+  "/resources/js/mynotifications.js",
+  "/resources/js/navbar-script.js",
+  "/resources/js/role.js",
+  "/resources/js/shoppingCart.js",
+  "/resources/js/urlHelper.js",
+  "/resources/js/client/addToCartButton.js",
+  "/resources/js/client/getSearchFlowers.js",
+  "/resources/js/loggedUser/changeData.js",
+  "/resources/js/notifications/deleteNotification.js",
+  "/resources/js/notlogged/getProducts.js",
+  "/resources/js/vendor/addFlower.js",
+  "/resources/js/vendor/deleteFlowers.js",
+  "/resources/js/vendor/getFlowers.js",
+  "/resources/js/vendor/moveToSale.js",
+];
+
+let whitelist_nocase= whitelist.map(value => value.toLowerCase());
+let pathsWhite_nocase=pathsCompletelyWhiteListed.map(value => value.toLowerCase());
+
+function isWithinWhitePath(str){
+  for(var pathW of pathsWhite_nocase)
+    if(str.toLowerCase().startsWith(pathW))
+      return true;
+  return false;
+}
+
+module.exports ={whitelist, whitelist_nocase, pathsWhite_nocase, pathsCompletelyWhiteListed, isWithinWhitePath} ;
